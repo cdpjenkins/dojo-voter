22f44e81d9a106b91dfa451fab1edb1236da833a
christianblunden <christian.blunden@gmail.com>
2013-05-14 10:45:47 +0100

    add production mysql database

--- src/dojo_voter/db/entities.clj
+++ src/dojo_voter/db/entities.clj
@@ -2,5 +2,11 @@
   (:use [korma.db]
         [korma.core]))
 
-(defdb dev (mysql {:db "dojo-voter" :host "localhost" :user "root" :password ""}))
+(defn prod? [] (= "production" (System/getenv "APP_ENV")))
+
+(def dbconfig (if (prod?) 
+          {:db "heroku_0f35f4734e8d48c" :host "us-cdbr-east-03.cleardb.com" :user "b92a75f2140f1f" :password (System/getenv "DATABASE_PASSWORD")}
+          {:db "dojo-voter" :host "localhost" :user "root" :password ""}))
+
+(defdb db (mysql dbconfig))
 (defentity topics)
