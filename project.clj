8fbe87bd981fe6d5542e9c6da1ba55b9bac6992b
christianblunden <christian.blunden@gmail.com>
2013-05-14 10:52:11 +0100

    use environ for environment variables

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [korma "0.3.0-RC5"]
-                           [mysql/mysql-connector-java "5.1.6"]]
-            :main dojo-voter.server)
+                           [mysql/mysql-connector-java "5.1.6"]
+                           [environ "0.4.0"]]
+            :main dojo-voter.server
+            :profiles {:production {:env {:production true}}})
 
--- src/dojo_voter/db/entities.clj
+++ src/dojo_voter/db/entities.clj
@@ -1,11 +1,10 @@
 (ns dojo-voter.db.entities
   (:use [korma.db]
-        [korma.core]))
+        [korma.core]
+        [environ.core]))
 
-(defn prod? [] (= "production" (System/getenv "APP_ENV")))
-
-(def dbconfig (if (prod?) 
-          {:db "heroku_0f35f4734e8d48c" :host "us-cdbr-east-03.cleardb.com" :user "b92a75f2140f1f" :password (System/getenv "DATABASE_PASSWORD")}
+(def dbconfig (if (env :production) 
+          {:db "heroku_0f35f4734e8d48c" :host "us-cdbr-east-03.cleardb.com" :user "b92a75f2140f1f" :password (env :database-password)}
           {:db "dojo-voter" :host "localhost" :user "root" :password ""}))
 
 (defdb db (mysql dbconfig))
